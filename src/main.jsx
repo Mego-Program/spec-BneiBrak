92c3f9450bac5e2bfbbcf1c341aea1e04865c4c4
unknown <pinikaminer8@gmail.com>
2023-11-05 13:49:50 +0200

    updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import App from "./components/App";
 import theme from "./theme";
 import { ThemeProvider } from "@mui/system";
 import CssBaseline from '@mui/material/CssBaseline';
 
-ReactDOM.render(
-    <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App />
-    </ThemeProvider>
-,
- document.getElementById("root"));
 
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+);
 
 
