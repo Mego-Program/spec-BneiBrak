0c077b70de1e79243c1c14cc29d5ebaae6e75b20
meirasulinn <96493389+meirasulinn@users.noreply.github.com>
2023-11-22 18:34:13 +0200

    added draft js with functional  component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import SpecList from './specList';
 import { Container } from '@mui/material';
+import RichTextEditor from './RichTextEditor';
 
 function App() {
   return (
     <Container>
       <SpecList />
+      {/* <RichTextEditor/> */}
+      
     </Container>
   );
 }
