c8bda48c714cd1be147efa21967c2e35287d3d82
hadaon <hadadon86@gmail.com>
2023-12-03 15:06:03 +0200

    Your commit message here

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import SpecList from './specList';
 import { Container } from '@mui/material';
+// import editKPIDetailsComponent from "./EditKPIDetailsComponent";
+import ContentEditorComponent from "./ContentEditorComponent";
+import DeleteSpecComponent from "./DeleteSpecComponent"; 
+import SpecTabs from './SpecTabs';
+import AlertDialog from './AlertDialog';
 
 function App() {
   return (
     <Container>
-      <SpecList />
+      {/* <AlertDialog/> */}
+      <SpecTabs />
+      {/* <SpecList/> */}
     </Container>
   );
 }
 
 export default App;
+
--- src/components/specList.jsx
+++ src/components/specList.jsx
@@ -1,76 +1,73 @@
-import React, { useState} from 'react';
-import { Grid, Button, Typography } from '@mui/material';
-import SpecItem from './specItem';
-import AddBox from '@mui/icons-material/AddBox';
-import Timeline from '@mui/lab/Timeline';
-import TimelineItem from '@mui/lab/TimelineItem';
-import TimelineSeparator from '@mui/lab/TimelineSeparator';
-import TimelineConnector from '@mui/lab/TimelineConnector';
-import TimelineContent from '@mui/lab/TimelineContent';
-import TimelineDot from '@mui/lab/TimelineDot';
+import React, { useState } from "react";
+import { Grid, Button, Typography } from "@mui/material";
+import SpecItem from "./specItem";
+import AddBox from "@mui/icons-material/AddBox";
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineDot from "@mui/lab/TimelineDot";
 
 function SpecList() {
   //Here is the code that will call and get the information via api
 
-      // const [teamSpecs, setTeamSpecs] = useState([]);
+  // const [teamSpecs, setTeamSpecs] = useState([]);
 
-      // useEffect(() => {
-      //   fetch('your-api-endpoint')
-      //     .then(response => response.json())
-      //     .then(data => setTeamSpecs(data))
-      //     .catch(error => console.error('Error fetching data:', error));
-      // }, []);
-
-
+  // useEffect(() => {
+  //   fetch('your-api-endpoint')
+  //     .then(response => response.json())
+  //     .then(data => setTeamSpecs(data))
+  //     .catch(error => console.error('Error fetching data:', error));
+  // }, []);
 
   const [teamSpecs, setTeamSpecs] = useState([
     {
       id: 1,
-      title: 'Team Spec 1',
+      title: "Team Spec 1",
       content:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae vitae nibh purus non dictum turpis leo, quis nam. Hac sed lectus est id. Nisi vestibulum, placerat integer nam nam. Blandit sagittis.',
-      date: '12.12.23',
-      status: 'In progress',
-      participants: ['A', 'B', 'C', 'D'],
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae vitae nibh purus non dictum turpis leo, quis nam. Hac sed lectus est id. Nisi vestibulum, placerat integer nam nam. Blandit sagittis.",
+      date: "12.12.23",
+      status: "In progress",
+      participants: ["A", "B", "C", "D"],
     },
     {
       id: 2,
-      title: 'Team Spec 2',
+      title: "Team Spec 2",
       content:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae vitae nibh purus non dictum turpis leo, quis nam. Hac sed lectus est id. Nisi vestibulum, placerat integer nam nam. Blandit sagittis.',
-      date: '13.12.23',
-      status: 'Done',
-      participants: ['A', 'B', 'C', 'D'],
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae vitae nibh purus non dictum turpis leo, quis nam. Hac sed lectus est id. Nisi vestibulum, placerat integer nam nam. Blandit sagittis.",
+      date: "13.12.23",
+      status: "Done",
+      participants: ["A", "B", "C", "D"],
     },
     {
       id: 3,
-      title: 'Team Spec 3',
+      title: "Team Spec 3",
       content:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae vitae nibh purus non dictum turpis leo, quis nam. Hac sed lectus est id. Nisi vestibulum, placerat integer nam nam. Blandit sagittis.',
-      date: '14.12.23',
-      status: 'In progress',
-      participants: ['A', 'B', 'C', 'D'],
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae vitae nibh purus non dictum turpis leo, quis nam. Hac sed lectus est id. Nisi vestibulum, placerat integer nam nam. Blandit sagittis.",
+      date: "14.12.23",
+      status: "In progress",
+      participants: ["A", "B", "C", "D"],
     },
     {
       id: 4,
-      title: 'Team Spec 4',
+      title: "Team Spec 4",
       content:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae vitae nibh purus non dictum turpis leo, quis nam. Hac sed lectus est id. Nisi vestibulum, placerat integer nam nam. Blandit sagittis.',
-      date: '15.12.23',
-      status: 'Done',
-      participants: ['A', 'B', 'C', 'D'],
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae vitae nibh purus non dictum turpis leo, quis nam. Hac sed lectus est id. Nisi vestibulum, placerat integer nam nam. Blandit sagittis.",
+      date: "15.12.23",
+      status: "Done",
+      participants: ["A", "B", "C", "D"],
     },
     {
       id: 5,
-      title: 'Team Spec 5',
+      title: "Team Spec 5",
       content:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae vitae nibh purus non dictum turpis leo, quis nam. Hac sed lectus est id. Nisi vestibulum, placerat integer nam nam. Blandit sagittis.',
-      date: '16.12.23',
-      status: 'In progress',
-      participants: ['A', 'B', 'C', 'D'],
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae vitae nibh purus non dictum turpis leo, quis nam. Hac sed lectus est id. Nisi vestibulum, placerat integer nam nam. Blandit sagittis.",
+      date: "16.12.23",
+      status: "In progress",
+      participants: ["A", "B", "C", "D"],
     },
   ]);
-  
 
   return (
     <Grid container spacing={2}>
@@ -78,19 +75,33 @@ function SpecList() {
         <Timeline
           position="left"
           sx={{
-            width: '20px',
-            padding: '1px',
-            height: '100%',
-            marginTop: '75px',
+            width: "20px",
+            padding: "1px",
+            height: "100%",
+            marginTop: "75px",
           }}
         >
           {teamSpecs.map((spec, index) => (
-            <TimelineItem key={spec.id} sx={{ minHeight: `${100 / teamSpecs.length - 2}%` }}>
+            <TimelineItem
+              key={spec.id}
+              sx={{ minHeight: `${100 / teamSpecs.length - 2}%` }}
+            >
               <TimelineSeparator>
-                <TimelineDot color="secondary"/>
-                {index < teamSpecs.length && <TimelineConnector sx={{ backgroundColor: 'secondary.main' }} />}
+                <TimelineDot color="secondary" />
+                {index < teamSpecs.length && (
+                  <TimelineConnector
+                    sx={{ backgroundColor: "secondary.main" }}
+                  />
+                )}
               </TimelineSeparator>
-              <TimelineContent sx={{ paddingLeft: '10px', paddingTop: '4px', color: 'white', opacity: spec.status === 'Done' ? 0.5 : 1 }}>
+              <TimelineContent
+                sx={{
+                  paddingLeft: "10px",
+                  paddingTop: "4px",
+                  color: "white",
+                  opacity: spec.status === "Done" ? 0.5 : 1,
+                }}
+              >
                 {spec.date}
               </TimelineContent>
             </TimelineItem>
@@ -104,31 +115,30 @@ function SpecList() {
               variant="contained"
               color="primary"
               style={{
-                width: '278px',
-                height: '55px',
-                borderRadius: '5px',
-                justifyContent: 'space-between',
-                marginLeft: '10px',
-                display: 'flex',
-                marginTop: '20px'
+                width: "278px",
+                height: "55px",
+                borderRadius: "5px",
+                justifyContent: "space-between",
+                marginLeft: "10px",
+                display: "flex",
+                marginTop: "20px",
               }}
             >
-              <Typography variant="button" sx={{ color: 'secondary.main' }}>
+              <Typography variant="button" sx={{ color: "secondary.main" }}>
                 Add a new spec
               </Typography>
               <div
                 style={{
-                  backgroundColor: 'secondary.main',
-                  borderRadius: '5px',
-                  padding: '6px',
-                  display: 'flex',
-                  alignItems: 'center',
-                  scale: '1.5',
-                  height: '100%',
-
+                  backgroundColor: "secondary.main",
+                  borderRadius: "5px",
+                  padding: "6px",
+                  display: "flex",
+                  alignItems: "center",
+                  scale: "1.5",
+                  height: "100%",
                 }}
               >
-                <AddBox sx={{height : '55px'}}/>
+                <AddBox sx={{ height: "55px" }} />
               </div>
             </Button>
           </Grid>
