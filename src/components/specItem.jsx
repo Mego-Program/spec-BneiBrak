2ff2c538c5fee5b1cfec3791c2d86e732ae652ad
asherf <319adf@gmail.com>
2023-12-06 12:02:45 +0200

    Added a delete button and added an edit button.

--- src/components/specItem.jsx
+++ src/components/specItem.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Card, CardContent, Typography, Grid, Button, Avatar, AvatarGroup } from '@mui/material';
 import { Link } from 'react-router-dom';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+
 
 
 export default function SpecItem({ spec }) {
@@ -18,28 +21,65 @@ export default function SpecItem({ spec }) {
         opacity: cardOpacity,
         width: '1168px',
         height: '143px',
-        bgcolor: "secondary.main", 
+        bgcolor: "secondary.main",
         color: "white",
-        
+
       }}
     >
-    <CardContent>
+      <CardContent>
         <Grid container >
           <Grid item xs={12}>
-            <Typography sx={{ fontSize: '25px',}}>{spec.title}</Typography>
+            <Typography sx={{ fontSize: '25px', }}>{spec.title}</Typography>
           </Grid>
           <Grid item xs={4}>
-            <Typography sx={{marginBottom: '60px',  fontSize: '12px'}}>{spec.content}</Typography>
+            <Typography sx={{ marginBottom: '60px', fontSize: '12px' }}>{spec.content}</Typography>
           </Grid>
           <Grid item xs={3}>
-            <Typography sx={{marginLeft: '120px', color: statusColor, }}>{spec.status}</Typography>
+            <Typography sx={{ marginLeft: '120px', color: statusColor, }}>{spec.status}</Typography>
           </Grid>
+
           <Grid item xs={3}>
-          <Button variant='text' size='small' sx={{color: 'white', marginLeft:'70px',}} component={Link} to={`/${spec.id}`}>Edit</Button>
+
+            <Button variant='text' size='small' sx={{
+              color: 'white',
+              height: '53',
+              width: '53',
+              minWidth: '0px',
+              borderRadius: '50%',
+              borderWidth: '1px',
+              borderStyle: 'solid',
+              borderColor: '#F6C927',
+              margin: '0 10px',
+
+              '&:hover': {
+                bgcolor: '#F6C927',
+              },
+            }} component={Link} to={'./SpecTabs'}>
+              <EditIcon />
+            </Button>
+            
+            <Button variant='text' size='small' sx={{
+              color: 'white',
+              height: '53',
+              width: '53',
+              minWidth: '0px',
+              borderRadius: '50%',
+              borderWidth: '1px',
+              borderStyle: 'solid',
+              borderColor: '#F6C927',
+              margin: '0 10px',
+
+              '&:hover': {
+                bgcolor: '#F6C927',
+              
+              },
+            }} component={Link} to={' '}>
+              <DeleteIcon />
+            </Button>
           </Grid>
           <AvatarGroup>
             {spec.participants.map((person, index) => (
-              <Avatar key={index} alt="User 1" sx={{bgcolor:"#121231", color:'#F6C927', }}>{person[0]}</Avatar>
+              <Avatar key={index} alt="User 1" sx={{ bgcolor: "#121231", color: '#F6C927', }}>{person[0]}</Avatar>
             ))}
           </AvatarGroup>
         </Grid>
